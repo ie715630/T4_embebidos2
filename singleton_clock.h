8283bd6ed8cb8ae3851a1c74ef1b44cdc9f576ab
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2020-09-28 13:40:28 -0500

    Added alarm headers

--- singleton_clock.h
+++ singleton_clock.h
@@ -11,20 +11,35 @@
 #include <stdint.h>
 #include "fsl_debug_console.h"
 
+typedef enum {FALSE, TRUE} bool_t;
+
 typedef struct {
     uint8_t seconds;
     uint8_t minutes;
+    uint8_t hours;
 } my_time_t;
 
+typedef struct {
+	bool_t sec_alarm;
+	bool_t min_alarm;
+	bool_t hour_alarm;
+} alarm_flag_t;
+
 void set_time(my_time_t time_to_set);
+void set_alarm (my_time_t time_to_set);
 void ten_ms_handler(void);
 void increment_seconds(void);
 void increment_minutes(void);
-void incremnet_hours(void);
+void increment_hours(void);
 
 uint8_t get_seconds(void);
 uint8_t get_minutes(void);
+uint8_t get_hours(void);
+
 void set_seconds(uint8_t sec);
 void set_minutes(uint8_t min);
+void set_hours(uint8_t hour);
+
+bool_t check_alarm(void);
 
 #endif /* SINGLETON_CLOCK_H_ */
